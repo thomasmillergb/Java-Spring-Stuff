aee2a182e6dc972357ace05ab937480a05b6d392
Thomas <thomasmillergb@outlook.com>
2016-01-31 23:55:13 +0000

    first commit

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php include 'common/head.php'; ?>
+<?php include 'common/header.php'; ?>
+
+
+<!-- Page Content -->
+<div class="container">
+
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">About Me
+            </h1>
+            <img class="profile-img img-rounded" src="./images/profile.JPG">
+            <p>I am landscape acrtictic who has been studying at the University of Sheffield</p>
+        </div>
+    </div>
+</div>
+    <!-- /.row -->
+<?php include 'common/footer.php'; ?>
---
+++ common/head.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>My Projects</title>
+
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <script src="http://code.jquery.com/jquery-2.2.0.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Bootstrap Core CSS -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="../css/bootstrap.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <!-- Custom CSS -->
+    <link href="../css/1-col-portfolio.css" rel="stylesheet">
+
+    <link href="../css/mini.css" rel="stylesheet">
+</head>
+
+<body>
